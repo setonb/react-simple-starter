3f414b2dd7e395c4a1206d61afd8d7136fb62eae
Seton Brown <setonb@gmail.com>
2017-06-27 12:42:15 -0400

    sending state through props to a child component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 // import Components
-import SearchBar from './components/search-bar'
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 // Youtube API KEY
 const API_KEY = "AIzaSyCj992jzxs4-SZhLDITW_fwy6zRGXqVMpk";
@@ -14,21 +15,22 @@ class App extends Component {
   constructor(props) {
       super(props);
 
+      this.state = {
+        videos: []
+      };
+
       // Initially populatee the videos array
       YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
         // compact using ES6 if key and property are same variable name
         this.setState({ videos });
       });
-
-      this.state = {
-        videos: []
-      };
   }
 
   render() {
     return (
         <div>
-          <SearchBar/>
+          <SearchBar />
+          <VideoList videos={this.state.videos} />
         </div>
     );
   }
